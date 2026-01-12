a437c886414a6a6f3ce39eab64895b24cfa659d1
Bassist_Cho <whgusghkd777@gmail.com>
2026-01-12 18:37:28 +0900

    Update MuSuBiApplication.java

--- src/main/java/com/example/musubi/MuSuBiApplication.java
+++ src/main/java/com/example/musubi/MuSuBiApplication.java
@@ -1,10 +1,12 @@
-package com.example.musubi; 
+package com.example.musubi;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MuSuBiApplication {
     public static void main(String[] args) {
+        // アプリケーションの起動
         SpringApplication.run(MuSuBiApplication.class, args);
     }
 }
